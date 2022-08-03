ef699392f1e2d5b30206ed91e95d672b4205afde
Isara Chumsri <gobetter@hotmail.com>
2022-08-03 16:47:27 +0700

    Retrieving posts from firebase, Posts, Post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react';
+// import { useSession } from 'next-auth/react';
+import { onSnapshot, collection, query, orderBy } from '@firebase/firestore';
 import { SparklesIcon } from '@heroicons/react/outline';
-import React from 'react';
+
 import Input from './Input';
+import Post from './Post';
+
+import { db } from '../firebase';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  // MESSY WAY
+  // useEffect(() => {
+  //   const unsubscribe = onSnapshot(
+  //     query(collection(db, "posts"), orderBy("timestamp", "desc")),
+  //     (snapshot) => {
+  //       setPosts(snapshot.docs);
+  //     }
+  //   );
+
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [db]);
+
+  // CLEAN WAY
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    []
+    // [db]
+  );
+
   return (
     <div className='text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
       <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700'>
@@ -13,6 +48,12 @@ const Feed = () => {
       </div>
 
       <Input />
+
+      <div className='pb-72'>
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 };
