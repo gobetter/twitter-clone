dd1a45b1748c8df884afa00da497a2ce329dcb7d
Isara Chumsri <gobetter@hotmail.com>
2022-08-03 11:58:00 +0700

    NextAuth, Login Page, Google Cloud Console

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head';
+import { getProviders, getSession, useSession } from 'next-auth/react';
+
 import Feed from '../components/Feed';
-
 import Sidebar from '../components/Sidebar';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div>
       <Head>
@@ -22,3 +28,25 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  );
+
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  );
+
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+};
